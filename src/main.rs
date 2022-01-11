02f5c8072a2079bcddf03897109102a4456d14ab
Guilherme Pellizzetti <guipellizzetti@gmail.com>
2022-01-10 22:12:06 -0300

    handle map in a separate file

--- src/main.rs
+++ src/main.rs
@@ -3,95 +3,16 @@ extern crate lazy_static;
 
 use bracket_terminal::prelude::*;
 
+use map::Map;
+
+mod map;
+
 bracket_terminal::add_wasm_support!();
 
 const CONSOLE_WIDTH: u32 = 80;
 const CONSOLE_HEIGHT: u32 = 50;
 const MAP_WIDTH: u32 = 80;
 const MAP_HEIGHT: u32 = 45;
-
-lazy_static! {
-    static ref FLOOR_TILE: Tile =
-        Tile::new(true, Graphic::new(' ', (255, 255, 255), (50, 50, 150)),);
-    static ref WALL_TILE: Tile = Tile::new(false, Graphic::new('#', (255, 255, 255), (0, 0, 100)),);
-}
-
-#[derive(Copy, Clone)]
-struct Graphic {
-    ch: char,
-    fg: (u8, u8, u8),
-    bg: (u8, u8, u8),
-}
-
-impl Graphic {
-    pub fn new(ch: char, fg: (u8, u8, u8), bg: (u8, u8, u8)) -> Graphic {
-        Graphic {
-            ch: ch,
-            fg: fg,
-            bg: bg,
-        }
-    }
-}
-
-#[derive(Copy, Clone)]
-struct Tile {
-    walkable: bool,
-    graphic: Graphic,
-}
-
-impl Tile {
-    pub fn new(walkable: bool, graphic: Graphic) -> Tile {
-        Tile {
-            walkable: walkable,
-            graphic: graphic,
-        }
-    }
-}
-
-struct Map {
-    width: u32,
-    height: u32,
-    tiles: Box<[Tile]>,
-}
-
-impl Map {
-    pub fn new(width: u32, height: u32) -> Map {
-        let map_size = (width * height) as usize;
-        let mut tiles = vec![*FLOOR_TILE; map_size].into_boxed_slice();
-
-        // Create some walls for testing collision
-        {
-            let walls = &mut tiles[330..360];
-            for x in 0..10 {
-                walls[x] = *WALL_TILE;
-            }
-        }
-
-        Map {
-            width: width,
-            height: height,
-            tiles: tiles,
-        }
-    }
-    pub fn map_idx(&self, x: u32, y: u32) -> usize {
-        ((y * self.width) + x) as usize
-    }
-    pub fn render(&self, ctx: &mut BTerm) {
-        ctx.set_active_console(0);
-        for y in 0..self.height {
-            for x in 0..self.width {
-                let index = self.map_idx(x, y);
-                ctx.set(
-                    x,
-                    y,
-                    self.tiles[index].graphic.fg,
-                    self.tiles[index].graphic.bg,
-                    to_cp437(self.tiles[index].graphic.ch),
-                );
-            }
-        }
-    }
-}
 
 struct Player {
     x: u32,
---
+++ src/map.rs
@@ -0,0 +1,75 @@
+use bracket_terminal::prelude::*;
+
+lazy_static! {
+  static ref FLOOR_TILE: Tile = Tile::new(true, Graphic::new(' ', (255, 255, 255), (50, 50, 150)),);
+  static ref WALL_TILE: Tile = Tile::new(false, Graphic::new('#', (255, 255, 255), (0, 0, 100)),);
+}
+
+#[derive(Copy, Clone)]
+pub struct Graphic {
+  ch: char,
+  fg: (u8, u8, u8),
+  bg: (u8, u8, u8),
+}
+
+impl Graphic {
+  pub fn new(ch: char, fg: (u8, u8, u8), bg: (u8, u8, u8)) -> Graphic {
+    Graphic {
+      ch: ch,
+      fg: fg,
+      bg: bg,
+    }
+  }
+}
+
+#[derive(Copy, Clone)]
+pub struct Tile {
+  pub walkable: bool,
+  pub graphic: Graphic,
+}
+
+impl Tile {
+  pub fn new(walkable: bool, graphic: Graphic) -> Tile {
+    Tile {
+      walkable: walkable,
+      graphic: graphic,
+    }
+  }
+}
+
+pub struct Map {
+  width: u32,
+  height: u32,
+  pub tiles: Box<[Tile]>,
+}
+
+impl Map {
+  pub fn new(width: u32, height: u32) -> Map {
+    let map_size = (width * height) as usize;
+    let tiles = vec![*FLOOR_TILE; map_size].into_boxed_slice();
+
+    Map {
+      width: width,
+      height: height,
+      tiles: tiles,
+    }
+  }
+  pub fn map_idx(&self, x: u32, y: u32) -> usize {
+    ((y * self.width) + x) as usize
+  }
+  pub fn render(&self, ctx: &mut BTerm) {
+    ctx.set_active_console(0);
+    for y in 0..self.height {
+      for x in 0..self.width {
+        let index = self.map_idx(x, y);
+        ctx.set(
+          x,
+          y,
+          self.tiles[index].graphic.fg,
+          self.tiles[index].graphic.bg,
+          to_cp437(self.tiles[index].graphic.ch),
+        );
+      }
+    }
+  }
+}
